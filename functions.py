3df482faaaa1698403e0131a12818d16ceeda844
NikhilB19 <nban982@aucklanduni.ac.nz>
2021-08-12 14:47:37 +1200

    added temperature ODE.

--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
 
 
 
-def odePressure(t, P, q, a, b, P0):
+def odePressure(t, P, a, b, q, P0):
     ''' Return the derivative dP/dt at a time, t for given parameters.
 
         Parameters:
@@ -10,12 +10,12 @@ def odePressure(t, P, q, a, b, P0):
             Independent variable.
         P : float
             Dependent varaible.
-        q : float
-            Source/sink rate.
         a : float
             Source/sink strength parameter.
         b : float
             Recharge strength parameter.
+        q : float
+            Source/sink rate.
         P0 : float
             Initial Pressure value.
 
@@ -36,3 +36,78 @@ def odePressure(t, P, q, a, b, P0):
 
     dPdt = -a*q - b*(P - P0)
     return dPdt
+
+def odeTemp(t, T, a, b, P, qsteam, T0, P0, M0, Tsteam, Tdash, bT):
+    ''' Return the derivative dT/dt at a time, t for given parameters.
+
+        Parameters:
+        -----------
+        t : float
+            Independent variable.
+        T : float
+            Dependent varaible.
+        a : float
+            Source/sink strength parameter.
+        b : float
+            Recharge strength parameter.
+        P : float
+            Pressure value.
+        qsteam : float
+            steam rate
+        T0 : float
+            initial Temperature
+        P0 : float
+            initial Pressure
+        M0 : float
+            initial Mass
+        Tsteam : float
+            steam Temperature?
+        Tdash : float
+            function returning value for T'(t)
+        bT : float
+            conduction parameter
+
+        Returns:
+        --------
+        dTdt : float
+            Derivative of dependent variable with respect to independent variable.
+
+        Notes:
+        ------
+        None
+
+        Examples:
+        ---------
+        >>> 
+
+    '''
+    Tprime = Tdash(t, P, P0)
+    dTdt = qsteam/M0 * (Tsteam - T) - b/(a*M0) * (P - P0) * (Tprime - T0) - bT*(T - T0)
+
+    return dTdt
+
+def Tprime(t, P, T, P0, T0):
+    ''' Return the current Temperature if current Pressure is more than initial Pressure, initial Temperature otherwise
+
+        Parameters:
+        -----------
+        t : float
+            current time
+        P : float
+            current Pressure
+        T : float
+            current Temperature
+        P0 : float
+            initial Pressure
+        T0 : float
+            initial Temperature
+        
+        Returns:
+        --------
+        Tprime : float
+            Returns the required value for temperature depending on directin of flow.
+    '''
+    if (P > P0):
+        return T
+    else:
+        return T0
